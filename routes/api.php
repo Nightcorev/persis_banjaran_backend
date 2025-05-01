92d9257c23650ff934754048ebd755870913ccec
ghifariwandanaa <ghifariwandana@gmail.com>
2025-05-02 00:35:48 +0700

    fix api hasil merge

--- routes/api.php
+++ routes/api.php
@@ -1,48 +1,64 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\Api\IuranController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PesantrenController;
+use App\Http\Controllers\IuranController as ControllersIuranController;
 use App\Http\Controllers\JamaahController;
 use App\Http\Controllers\JamaahMonografiController;
+use App\Http\Controllers\PesantrenController;
 use App\Http\Controllers\JamaahFasilitasController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\IuranController as ControllersIuranController;
+use App\Http\Controllers\MusyawarahController;
 use App\Http\Controllers\BroadcastController;
 use App\Http\Controllers\ResponBotController;
-use App\Http\Controllers\MusyawarahController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
 */
 
-// ---------- AUTH ----------
+// Route untuk login dan logout
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth.token');
 
-// ---------- PROTECTED ROUTES ----------
+// Routes yang membutuhkan auth.token middleware
 Route::middleware('auth.token')->group(function () {
-
-    // ---------- ANGGOTA ----------
+    // Route untuk Anggota
     Route::get('/anggota', [AnggotaController::class, 'index']);
     Route::get('/anggota/all', [AnggotaController::class, 'selectAll']);
     Route::get('/anggota/{id}', [AnggotaController::class, 'show']);
-    Route::post('/anggota', [AnggotaController::class, 'store'])->middleware('permission:data_anggota,add');
-    Route::put('/anggota/{id}', [AnggotaController::class, 'update'])->middleware('permission:data_anggota,edit');
-    Route::delete('/anggota/{id}', [AnggotaController::class, 'destroy'])->middleware('permission:data_anggota,delete');
+
+    Route::post('/anggota', [AnggotaController::class, 'store'])
+        ->middleware('permission:data_anggota,add');
+    Route::put('/anggota/{id}', [AnggotaController::class, 'update'])
+        ->middleware('permission:data_anggota,edit');
+    Route::delete('/anggota/{id}', [AnggotaController::class, 'destroy'])
+        ->middleware('permission:data_anggota,delete');
+    // Route::get('/anggota/by-jamaah/{id_master_jamaah?}', [AnggotaController::class, 'indexByJamaah']);
+
     Route::get('/anggota/choice_by-jamaah/{id_master_jamaah?}', [AnggotaController::class, 'anggotaByJamaah']);
+
     Route::post('/upload-foto', [AnggotaController::class, 'uploadFoto']);
 
-    // ---------- DATA & STATISTIK ----------
+    // Route untuk Data Jamaah dan Statistik
+    // Route::get('/data_jamaah', [JamaahMonografiController::class, 'index']);
     Route::get('/data_chart', [AnggotaController::class, 'chart']);
     Route::get('/data_monografi', [AnggotaController::class, 'statistik']);
     Route::get('/jamaah-monografi/{id_master_jamaah}', [JamaahMonografiController::class, 'show']);
+
+    // Route untuk Data Pilihan DropDown
     Route::get('/data_choice_pribadi', [AnggotaController::class, 'getChoiceDataPribadi']);
     Route::get('/data_choice_pendidikan', [AnggotaController::class, 'getChoiceDataPendidikan']);
     Route::get('/data_choice_pekerjaan', [AnggotaController::class, 'getChoiceDataPekerjaan']);
@@ -50,11 +66,15 @@ Route::middleware('auth.token')->group(function () {
     Route::get('/data_choice_minat', [AnggotaController::class, 'getChoiceDataMinat']);
     Route::get('/data_choice_jamaah', [JamaahMonografiController::class, 'getChoiceDataJamaah']);
 
-    // ---------- PESANTREN & FASILITAS ----------
+    // Route untuk Webhooks
+    Route::get('webhooks', [WebhookController::class, 'verifyWebhook']);
+    Route::post('webhooks', [WebhookController::class, 'handleWebhook']);
+
+    // // Route tambahan dari feat/data_monografi
     Route::get('/pesantren/by-jamaah/{id_master_jamaah?}', [PesantrenController::class, 'indexByJamaah']);
     Route::get('/fasilitas/by-jamaah/{id_master_jamaah?}', [JamaahFasilitasController::class, 'indexByJamaah']);
 
-    // ---------- PERMISSION & ROLE ----------
+    // Route untuk Permissions
     Route::prefix('permissions')->group(function () {
         Route::get('/', [PermissionController::class, 'index']);
         Route::post('/', [PermissionController::class, 'store']);
@@ -62,6 +82,7 @@ Route::middleware('auth.token')->group(function () {
         Route::delete('/{id}', [PermissionController::class, 'destroy']);
     });
 
+    // Route untuk Roles
     Route::prefix('roles')->group(function () {
         Route::get('/', [RoleController::class, 'index']);
         Route::post('/', [RoleController::class, 'store']);
@@ -69,7 +90,7 @@ Route::middleware('auth.token')->group(function () {
         Route::delete('/{id}', [RoleController::class, 'destroy']);
     });
 
-    // ---------- USERS ----------
+    // Route untuk Users
     Route::prefix('users')->group(function () {
         Route::get('/', [UserController::class, 'index']);
         Route::post('/', [UserController::class, 'store']);
@@ -77,7 +98,7 @@ Route::middleware('auth.token')->group(function () {
         Route::delete('/{id}', [UserController::class, 'destroy']);
     });
 
-    // ---------- IURAN ----------
+    // Endpoint Iuran
     Route::prefix('iuran')->group(function () {
         Route::get('/summary', [ControllersIuranController::class, 'summary']);
         Route::get('/payment/{id}', [ControllersIuranController::class, 'paymentDetail']);
@@ -90,43 +111,65 @@ Route::middleware('auth.token')->group(function () {
         Route::get('/tunggakan', [ControllersIuranController::class, 'getTunggakan']);
     });
 
-    // ---------- CHATBOT ----------
-    Route::prefix('chatbot')->group(function () {
-        Route::get('/', [ResponBotController::class, 'index']);
-        Route::post('/', [ResponBotController::class, 'store']);
-        Route::put('/{responBot}', [ResponBotController::class, 'update']);
-        Route::get('/{responBot}', [ResponBotController::class, 'show']);
-        Route::delete('/{responBot}', [ResponBotController::class, 'destroy']);
-    });
+    // Route::prefix('iuran')->group(function () {
+    //     Route::get('/summary', [IuranController::class, 'summary'])
+    //          ->middleware('permission:iuran,view');
+    //     Route::get('/payment/{id}', [IuranController::class, 'paymentDetail'])
+    //          ->middleware('permission:iuran,view');
+    //     Route::post('/pay', [IuranController::class, 'store'])
+    //          ->middleware('permission:iuran,create');
+    //     Route::put('/edit/{id}', [IuranController::class, 'updateNominal'])
+    //          ->middleware('permission:iuran,update');
+    //     Route::put('/verify/{id}', [IuranController::class, 'verify'])
+    //          ->middleware('permission:iuran,verify');
+    //     Route::put('/reject/{id}', [IuranController::class, 'reject'])
+    //          ->middleware('permission:iuran,reject');
+    //     Route::delete('/{id}', [IuranController::class, 'destroy'])
+    //          ->middleware('permission:iuran,delete');
+    //     Route::post('/reminder/batch', [IuranController::class, 'sendBatchReminder'])
+    //          ->middleware('permission:iuran,send_reminder');
 
-    // ---------- BROADCAST ----------
+    //     // --- RUTE BARU UNTUK DATA TUNGGAKAN ---
+    //     Route::get('/tunggakan', [IuranController::class, 'getTunggakan'])
+    //          ->middleware('permission:iuran,view'); // Atau permission 'send_reminder'
+    //     // --- AKHIR RUTE BARU ---
+    // });
+
+    // --- Endpoint Chatbot (BARU) ---
+    Route::get('/chatbot', [ResponBotController::class, 'index']); // Kecualikan aksi yg butuh permission beda
+
+    Route::post('/chatbot', [ResponBotController::class, 'store']);
+
+    Route::put('/chatbot/{responBot}', [ResponBotController::class, 'update']);
+
+    Route::get('chatbot/{responBot}', [ResponBotController::class, 'show']);
+
+    Route::delete('/chatbot/{responBot}', [ResponBotController::class, 'destroy']);
+
     Route::get('/broadcast', [BroadcastController::class, 'index']);
     Route::post('/broadcast', [BroadcastController::class, 'store']);
     Route::post('/upload-attachment', [BroadcastController::class, 'uploadAttachment']);
-
-    // ---------- WEBHOOK ----------
-    Route::get('/webhooks', [WebhookController::class, 'verifyWebhook']);
-    Route::post('/webhooks', [WebhookController::class, 'handleWebhook']);
+    // --- AKHIR Endpoint Chatbot ---
 });
 
-// ---------- PUBLIC / TESTING ROUTES (opsional, bisa dihapus jika tidak perlu) ----------
+
+Route::get('/anggota', [AnggotaController::class, 'index']);
 Route::get('/get_anggota/{id}', [AnggotaController::class, 'show']);
 Route::post('/add_anggota', [AnggotaController::class, 'store']);
 Route::put('/edit_anggota/{id}', [AnggotaController::class, 'update']);
 Route::delete('/delete_anggota/{id}', [AnggotaController::class, 'destroy']);
+
 Route::get('/advanced_statistic', [AnggotaController::class, 'advancedStatistic']);
 
-// ---------- MUSYAWARAH ----------
-Route::get('/musyawarah', [MusyawarahController::class, 'index']);
-Route::get('/musyawarah/{id}', [MusyawarahController::class, 'view']);
-Route::post('/musyawarah', [MusyawarahController::class, 'store']);
-Route::put('/musyawarah/{id}', [MusyawarahController::class, 'update']);
-Route::delete('/musyawarah/{id}', [MusyawarahController::class, 'destroy']);
+Route::get('/data_musyawarah', [MusyawarahController::class, 'index']);
+Route::get('/detail_musyawarah/{id}', [MusyawarahController::class, 'view']);
+Route::post('/add_musyawarah', [MusyawarahController::class, 'store']);
+Route::put('/edit_musyawarah/{id}', [MusyawarahController::class, 'update']);
+Route::delete('/delete_musyawarah/{id}', [MusyawarahController::class, 'destroy']);
+Route::get('/data_jamaah', [JamaahMonografiController::class, 'index']);
+Route::get('/anggota/by-jamaah/{id_master_jamaah?}', [AnggotaController::class, 'indexByJamaah']);
+
 Route::post('/musyawarah/detail/{id_musyawarah}', [MusyawarahController::class, 'addDetail']);
 Route::put('/musyawarah/detail/{id_musyawarah}/{id_detail}', [MusyawarahController::class, 'updateDetail']);
 Route::delete('/musyawarah/detail/{id_musyawarah}/{id_detail}', [MusyawarahController::class, 'destroyDetail']);
 Route::get('/musyawarah/detail/{id_musyawarah}/{id_detail}', [MusyawarahController::class, 'showDetail']);
-
-// ---------- JEMAAH DATA ----------
-Route::get('/data_jamaah', [JamaahMonografiController::class, 'index']);
-Route::get('/anggota/by-jamaah/{id_master_jamaah?}', [AnggotaController::class, 'indexByJamaah']);
